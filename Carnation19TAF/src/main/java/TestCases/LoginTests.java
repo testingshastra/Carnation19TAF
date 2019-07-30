7aa308b10fb21b52976fb28e674fedbef4d11750
pritesh848 <pritesh848@gmail.com>
2019-07-30 19:35:11 +0530

    logo

--- Carnation19TAF/src/main/java/TestCases/LoginTests.java
+++ Carnation19TAF/src/main/java/TestCases/LoginTests.java
@@ -1,5 +1,29 @@
 package TestCases;
 
 public class LoginTests {
+WebDriver driver;
+	@BeforeMethod
+public void startUp() {
+	System.setProperty("webdriver.chrome.driver","./Driver\\chromedriver.exe");
+	 driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	
+}
+	/**
+	 * This test can check that logo is displaying or not .
+	 * if logo is displaying then it will make b  (boolean ) as  true .
+	 * @author pritesh
+	 * 
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void logoTest() throws InterruptedException {
+		driver.get("https://eagercrow.com/Home");
+		Thread.sleep(2000);
+		boolean b = driver.findElement(By.xpath("//a[@class='logo active']/img[@alt='IMG-LOGO']")).isDisplayed();
+		System.out.println(b);
+
+	}
+
 
 }
