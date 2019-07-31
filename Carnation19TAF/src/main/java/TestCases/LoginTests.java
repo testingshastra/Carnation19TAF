400a60e6a4df44d1356fe6fd0fd53625e24e9697
pritesh848 <pritesh848@gmail.com>
2019-07-31 09:17:40 +0530

    second commit by pritesh

--- Carnation19TAF/src/main/java/TestCases/LoginTests.java
+++ Carnation19TAF/src/main/java/TestCases/LoginTests.java
@@ -1,12 +1,24 @@
 package TestCases;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
 
 public class LoginTests {
 WebDriver driver;
-	@BeforeMethod
+	@BeforeTest
 public void startUp() {
 	System.setProperty("webdriver.chrome.driver","./Driver\\chromedriver.exe");
 	 driver = new ChromeDriver();
 	driver.manage().window().maximize();
+	
+	driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 	
 }
 	/**
@@ -16,14 +28,30 @@ public void startUp() {
 	 * 
 	 * @throws InterruptedException
 	 */
-	@Test
+	@Test(priority=1)
 	public void logoTest() throws InterruptedException {
 		driver.get("https://eagercrow.com/Home");
-		Thread.sleep(2000);
+		
+		
 		boolean b = driver.findElement(By.xpath("//a[@class='logo active']/img[@alt='IMG-LOGO']")).isDisplayed();
 		System.out.println(b);
 
+		
 	}
+	/**
+	 * this method is used to test the sign in with email and passsword
+	 * 
+	 * @author pritesh
+	 * @throws InterruptedException
+	 */
+	@Test(priority= 2)
+	public void signin() throws InterruptedException {
 
-
+		driver.findElement(By.xpath("//div[@class='header-icons']/a[@routerlink='/SignIn']")).click();
+		driver.findElement(By.xpath("//span[contains(text(),'Sign in with email')]")).click();
+		driver.findElement(By.xpath("//input[@name='email' and @autocomplete='username']")).sendKeys("abc@gmail.com");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Testing@123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+	}
 }
